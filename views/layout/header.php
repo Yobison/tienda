8c945396fe28274c83470d826af292d6b471b798
Jesús Pérez <yobison@gmail.com>
2023-09-29 21:13:45 +0200

    paginado, creado registro

---
+++ views/layout/header.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?=base_url?>assets/css/styles.css">
+    <title>Tienda de camisetas</title>
+</head>
+<body>
+    <div id="container">
+    <!--CABECERA-->
+
+        <header id="header">
+            <div id="logo">
+                <img src="<?=base_url?>assets/img/camiseta.png" alt="camiseta logo">
+                <a href="index.php">
+                    Tienda de camisetas
+                </a>
+            </div>
+
+        </header>
+
+        <!--MENU-->
+
+        <nav id="menu">
+            <ul>
+                <li>
+                    <a href="">Inicio</a>
+                </li>
+                <li>
+                    <a href="">Categoria 1</a>
+                </li>
+                <li>
+                    <a href="">Categoria 2</a>
+                </li>
+                <li>
+                    <a href="">Categoria 3</a>
+                </li>
+                <li>
+                    <a href="">Categoria 4</a>
+                </li>
+                <li>
+                    <a href="">Categoria 5</a>
+                </li>
+            </ul>
+        </nav>
+
+        <div id="content">
